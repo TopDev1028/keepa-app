1d4d114eb59c1d7939613373165d92a469e0b469
TopDev1028 <app.seed5406@gmail.com>
2023-09-12 09:39:12 +0900

    first commit

---
+++ api/engine.py
@@ -0,0 +1,135 @@
+import csv
+import requests
+from prettytable import PrettyTable
+import os
+import datetime
+from googletrans import Translator
+from tabulate import tabulate
+import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.environ["KEEPA_API"]
+translator = Translator()
+
+
+current_date = datetime.date.today()
+asin_result_file_name = "results\\asin\\" + current_date.strftime("%Y-%m-%d") + ".csv"
+
+
+def translate_en_es(strings):
+    translated_strings = strings
+    translate_ids = [
+        1,
+        2,
+        3,
+        4,
+        6,
+        len(strings) - 10,
+        len(strings) - 9,
+        len(strings) - 8,
+        len(strings) - 7,
+        len(strings) - 6,
+        len(strings) - 5,
+        len(strings) - 4,
+        len(strings) - 3,
+        len(strings) - 2,
+        len(strings) - 1,
+    ]
+    for id in translate_ids:
+        string = strings[id]
+        print(string)
+        # string = string.strip()
+        if string == "" or string == None:
+            translated_string = ""
+        else:
+            translation = translator.translate(string, dest="es", src="auto")
+
+        translated_string = translation.text if translation is not None else ""
+        translated_strings[id] = translated_string
+        print(translated_string)
+    return translated_strings
+
+
+def scrap():
+    asin_scrap()
+    filter_scrap()
+
+
+def asin_scrap():
+    ASINS = []
+    with open("settings\\asins.csv", "r") as file:
+        csv_reader = csv.reader(file)
+
+        for row in csv_reader:
+            ASINS.append(row[0])
+
+    params = {
+        "key": API_KEY,
+        "domain": "1",
+        "asin": ",".join(ASINS),
+        "stats": 30,
+    }
+
+    response = requests.get("https://api.keepa.com/product", params=params)
+
+    print(response.url)
+
+    # CSV header part
+    headers = [
+        "ASIN",
+        "Title",
+        "Description",
+        "Manufacturer",
+        "Brand",
+        "Model",
+        # "Color",
+        "Prime Eligible",
+        "Current",
+        "Average",
+        "30 Day Average",
+        "90 Day Average",
+        "180 Day Average",
+        "365 Day Average",
+    ]
+    attrs = ["asin", "title", "description", "manufacturer", "brand", "model"]
+    for i in range(1, 11):
+        headers.append("Feature " + str(i))
+
+    table_data = []
+    if response.status_code == 200:
+        products_data = response.json()["products"]
+        for product in products_data:
+            temp = []
+            features = [""] * 10
+
+            for attr in attrs:
+                temp.append(product[attr])
+            for index, value in enumerate(product["features"]):
+                features[index] = value
+            temp.append(product["stats"]["buyBoxIsPrimeEligible"])
+
+            # Price part
+            stats = product["stats"]
+
+            statsKey = ["current", "avg", "avg30", "avg90", "avg180", "avg365"]
+            for key in statsKey:
+                price = 0
+                for item in stats[key]:
+                    if item != -1 and item != None:
+                        price += item
+                print(price)
+                temp.append(str(price) + "$")
+
+            temp = temp + features
+            translated_temp = translate_en_es(temp)
+
+            table_data.append(translated_temp)
+        df = pd.DataFrame(table_data, columns=headers)
+        df.to_csv(asin_result_file_name, index=False, encoding="utf-8")
+    else:
+        print(f"Error: {response.status_code}")
+
+
+def filter_scrap():
+    pass
---
+++ api/main.py
@@ -0,0 +1,157 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse, FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+from apscheduler.schedulers.background import BackgroundScheduler
+from pydantic import BaseModel
+import threading
+import os
+import uvicorn
+import json
+
+from engine import *
+
+app = FastAPI()
+scheduler = BackgroundScheduler()
+is_work_service_running = False
+service_running = False
+thread = threading.Thread(target=scrap)
+
+setting = {}
+with open("settings\\settings.json") as file:
+    setting = json.load(file)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+def work_service():
+    print("Running work service...")
+    scrap()
+
+
+def start_service_background():
+    thread.start()
+
+
+@app.on_event("startup")
+async def startup_event():
+    scheduler.add_job(work_service, "cron", hour=2, minute=0, timezone="CST6CDT")
+    is_work_service_running = True
+    scheduler.start()
+
+
+@app.post("/asinupload")
+async def upload_file(file: UploadFile = File(...)):
+    valid_extensions = ["csv", "xlsx"]
+    file_extension = file.filename.split(".")[-1]
+    if file_extension not in valid_extensions:
+        return JSONResponse(
+            {"error": "Invalid file format. Only CSV and XLSX files are allowed."},
+            status_code=400,
+        )
+    file_path = os.path.join("settings", "asins.csv")
+    with open(file_path, "wb") as f:
+        f.write(await file.read())
+    return JSONResponse({"success": "true"})
+
+
+@app.post("/download")
+async def download_file(file_request: dict):
+    file_name = file_request["filename"]
+    file_path = f"results\\{file_name}"
+    return FileResponse(file_path, filename=file_name)
+
+
+@app.post("/start-scheduling-service")
+async def start_work_service():
+    global is_work_service_running
+    if not is_work_service_running:
+        is_work_service_running = True
+        scheduler.shutdown()
+        scheduler.start()
+    return {"message": "Work service started"}
+
+
+@app.post("/stop-scheduling-service")
+async def stop_work_service():
+    global is_work_service_running
+    if is_work_service_running:
+        is_work_service_running = False
+        scheduler.shutdown()
+    return {"message": "Work service stopped"}
+
+
+@app.post("/start-scraping")
+async def start_service():
+    global service_running
+    if not service_running:
+        service_running = True
+        thread.start()
+        return {"success": "1", "message": "Service started successfully"}
+    else:
+        return {"success": "2", "message": "Service is already running"}
+
+
+@app.post("/stop-scraping")
+async def start_service():
+    global service_running
+    if service_running:
+        service_running = False
+        thread.join()
+        return {"success": "1", "message": "Service stopped successfully"}
+    else:
+        return {"success": "2", "message": "Service is already stoppped"}
+
+
+@app.post("/get-file-info")
+async def get_file_info():
+    asin_files = []
+    filter_files = []
+    asin_path = "results\\asin"
+    asin_names = os.listdir(asin_path)
+    for asin_name in asin_names:
+        asin_files.append(asin_name)
+
+    filter_path = "results\\filter"
+    filter_names = os.listdir(asin_path)
+    for filter_name in filter_names:
+        filter_files.append(filter_name)
+
+    return {"asin": asin_files, "filter": filter_files}
+
+
+@app.post("/get-setting")
+async def get_setting():
+    return {"data": setting}
+
+
+@app.post("/save-filter")
+async def save_scheduling():
+    return {"success": True}
+
+
+class Schedule(BaseModel):
+    hour: int
+    minute: int
+
+
+@app.post("/save-schedule")
+def save_filter(data: Schedule):
+    hour = data.hour
+    minute = data.minute
+
+    setting["schedule"]["hour"] = hour
+    setting["schedule"]["minute"] = minute
+
+    with open("settings\\settings.json", "w") as file:
+        json.dump(setting, file)
+
+    return {"message": "Data received successfully"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
